959d80a5004d7079783bec45a25521e63f400133
zane <i.zane.am.robot@gmail.com>
2016-10-16 21:44:23 -0400

    Ran a few tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,50 @@
 #write your code here
+
+def add(x, y)
+	x + y
+end
+
+def subtract(x, y)
+	x - y
+end
+
+def sum(array)
+	sum = 0
+	array.each do |i|
+		sum += i
+	end
+	sum
+end
+
+
+
+def multiply(*args)
+	result = 1
+	args.each do |arg|
+		result = result * arg
+	end
+	result
+end
+def power(x, y)
+	x ** y
+end
+
+#Explanation for Factorial
+#while the x is positive
+#result becomes result times x
+#Decrease th enumber x by one
+#Once the number x becomes 0
+#The result is equal to the multiplication of all
+#numbers from 1 to what n was at the beginning
+def factorial(x)
+	result = 1
+	if x == 0
+		1
+	else
+		while x > 0
+			result = result * x
+			x -= 1
+		end
+		result
+	end
+end
